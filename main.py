223fcd1418a633fe0ff5392cab3b1c3361e0e99a
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-03-22 14:33:44 +0100

    Remove of loading factories from main

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from src.GUI.main_window import MainWindow
-from src.factory import FactoryLoader
 
 recipe_file_path = "data/recipe.json"
 
 def main():
-    factories = FactoryLoader.load(recipe_file_path)
-
     app = QApplication(sys.argv)
 
     window = MainWindow()
