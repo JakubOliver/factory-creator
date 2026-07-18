3d6fcad0551ed7243f5d399d219f831084e9083b
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-07-19 00:51:37 +0200

    Add comments to cryptic __init__.py of factory creator, which uses lazy import for GUI and CLI.
    Update main.py so it uses lazy imports for GUI (CLI does not make much difference).

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
-from PySide6.QtWidgets import QApplication
 
-from src.factory_creator.cli.argument_processor import ArgumentProcessor
-from src.factory_creator.cli.cli import CLI
+from src.factory_creator.cli import ArgumentProcessor, CLI
 #TODO: setup pyproject.toml (and use pip install -e .   so it is not needed to write src.factory... but only factory...)
-
-from src.factory_creator.gui.main_window import MainWindow
+#TODO: add option to set start and end of mutaiton in gui (and CLI)
 
 
 def main(no_browser: bool = False):
     """
     Main function which call the GUI.
     """
+    from PySide6.QtWidgets import QApplication
+    from src.factory_creator import MainWindow
+
     app = QApplication(sys.argv)
 
     window = MainWindow(use_embedded_browser=not no_browser)
--- src/factory_creator/__init__.py
+++ src/factory_creator/__init__.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING as _TYPE_CHECKING, Any as _Any
 
+# If IDE or some type checker is used, we need to import the public user interfaces here so that they are visible to the IDE.
 if _TYPE_CHECKING:
     from .cli import CLI
     from .gui import MainWindow
@@ -9,12 +10,15 @@ __all__ = ["CLI", "MainWindow"]
 
 def __getattr__(name: str) -> _Any:
     """Load the public user interfaces only when they are requested."""
+    # Uses lazy loading to reduce initial import time and loading unnecessary modules.
     if name == "CLI":
         from .cli import CLI
 
         return CLI
+
     if name == "MainWindow":
         from .gui import MainWindow
 
         return MainWindow
+
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
