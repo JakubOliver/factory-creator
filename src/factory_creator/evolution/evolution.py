5828bcd6df27d056c476f28da4ad238a3d739d26
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-07-18 14:11:00 +0200

    Create demo for new mutation.
    It is uses that DAG has several different topological orderings so it by random choose several and creates from them new grid.

--- src/factory_creator/evolution/evolution.py
+++ src/factory_creator/evolution/evolution.py
@@ -18,6 +18,7 @@ class Evolution:
         report_method: Callable[[str], None] = print,
     ) -> Grid | list[Grid]:
         # TODO: Add optional per-run fitness caching to avoid evaluating the same layout repeatedly.
+        # TODO: As its is currently implemented, the computation can be parallelized by running multipler hill climbs or mayber better parts of hill climbs in different threads and then combining the results.
         algorithm = HillClimbing(
             mutations=[MoveBuildingMutation(show_failure_reasons=True)],
             fitness=Fitness(),
--- src/factory_creator/graph_processing/__init__.py
+++ src/factory_creator/graph_processing/__init__.py
@@ -1,3 +1,4 @@
 from .graph_to_matrix import GraphToMatrix
+from .topological_sort_generator import TopologicalSortGenerator
 
-__all__ = ["GraphToMatrix"]
+__all__ = ["GraphToMatrix", "TopologicalSortGenerator"]
